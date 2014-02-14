c5a5376c2a6e5510b78de5af3eab805566289845
VegaFromLyra <asha.ab@gmail.com>
2014-02-13 22:52:17 -0800

    Arrange given numbers to form the biggest number
    The idea is to use any comparison based sorting algorithm. In the used
    sorting algorithm, instead of using the default comparison, write a
    comparison function myCompare() and use it to sort numbers. Given two
    numbers X and Y, how should myCompare() decide which number to put first
    – we compare two numbers XY (Y appended at the end of X) and YX (X
    appended at the end of Y). If XY is larger, then X should come before Y
    in output, else Y should come before. For example, let X and Y be 542
    and 60. To compare X and Y, we compare 54260 and 60542. Since 60542 is
    greater than 54260, we put Y first.

---
+++ CombineInts/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombineInts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 98, 1, 3, 34, 9, 76, 45, 4 };
+            // int[] arr = { 54, 546, 548, 60 };
+            Int64 result = CombineInts(arr);
+
+            Console.WriteLine(result);
+        }
+
+        static Int64 CombineInts(int[] arr)
+        {
+            string[] input = new string[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                input[i] = arr[i].ToString();
+            }
+
+            Array.Sort(input, CustomSort);
+
+            string result = null;
+
+            foreach (string str in input)
+            {
+                result += str;
+            }
+
+            return Int64.Parse(result);
+        }
+
+        static int CustomSort(string s1, string s2)
+        {
+            string s1s2 = s1 + s2;
+            string s2s1 = s2 + s1;
+
+            return s1s2.CompareTo(s2s1) > 0 ? -1 : 1;
+        }
+    }
+}
